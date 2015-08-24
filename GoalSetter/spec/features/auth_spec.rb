095ed3ba612247ae0af544fae9f4f1d8d853bb61
Caitlin and RJ <caitlin+rj@gmail.com>
2015-08-24 10:04:49 -0400

    Set up testing environment.

---
+++ GoalSetter/spec/features/auth_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require 'rails_helper'
+
+feature "the signup process" do
+  it "has a new user page"
+  feature "signing up a user" do
+    it "shows username on the homepage after signup"
+end
