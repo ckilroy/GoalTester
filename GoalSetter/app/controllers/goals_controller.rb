32de46e563df516f52d7cdb17f38cc8833c1399e
Caitlin and RJ <caitlin+rj@gmail.com>
2015-08-24 12:30:30 -0400

    Add Goal controller actions.

--- GoalSetter/app/controllers/goals_controller.rb
+++ GoalSetter/app/controllers/goals_controller.rb
@@ -1,5 +1,51 @@
 class GoalsController < ApplicationController
 
   def index
+    @goals = Goal.all
   end
+
+  def new
+    @goal = Goal.new
+  end
+
+  def create
+    @goal = current_user.goals.new(goal_params)
+
+    if @goal.save
+      redirect_to goal_url(@goal)
+    else
+      flash[:errors] = @goal.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    @goal = Goal.find(params[:id])
+  end
+
+  def update
+    @goal = Goal.find(params[:id])
+
+    if @goal.update(goal_params)
+      redirect_to goal_url(@goal)
+    else
+      flash[:errors] = @goal.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @goal = Goal.find(params[:id])
+    @goal.destroy!
+    redirect_to goals_url
+  end
+
+  def show
+    @goal = Goal.find(params[:id])
+  end
+
+  def goal_params
+    params.require(:goal).permit(:text, :private)
+  end
+
 end
